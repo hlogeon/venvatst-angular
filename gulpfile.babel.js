780ee4be0f5718fb7b05a20515f8ccfe9834259f
Gustavo Gard <gusgard@gmail.com>
2016-03-22 20:06:14 -0300

    Add browsersync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,26 +1,19 @@
 'use strict';
 
 import gulp from 'gulp';
-import babel from 'gulp-babel';
-import sass from 'gulp-sass';
-import sourcemaps from 'gulp-sourcemaps';
-// import rename from 'gulp-rename';
-import concat from 'gulp-concat';
-import uglify from 'gulp-uglify';
-import minifyCss from 'gulp-clean-css';
-import webserver from 'gulp-webserver';
-import livereload from 'gulp-livereload';
-import rev from 'gulp-rev-append';
 import sequence from 'run-sequence';
-// import ngAnnotate from 'gulp-ng-annotate';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
 
-let dirs = {
-  src: 'app',
-  dest: 'build'
-  // dest: 'dist'
-};
-
+let reload = browserSync.reload;
+let plugins = gulpLoadPlugins();
 // add browserify to use import *.
+
+const DIRS = {
+  src: 'app',
+  dest: 'build',
+  js: { app: 'js/app.js', libs: 'js/libs.js' }
+};
 
 const JS_DEPENDENCIES = [
   'node_modules/angular/angular.js',
@@ -34,70 +27,62 @@ const CSS_DEPENDENCIES = [
   'node_modules/bootstrap/dist/css/bootstrap-theme.css'
 ];
 
-let PATHS = {
-  sass: [`${dirs.src}/scss/*.scss`, `${dirs.src}/scss/**/*.scss`],
-  js: [`${dirs.src}/js/*.js`, `${dirs.src}/js/**/*.js`],
-  html: [`${dirs.src}/templates/*.html`, `${dirs.src}/templates/**/*.html`]
+const PATHS = {
+  sass: [`${DIRS.src}/scss/*.scss`, `${DIRS.src}/scss/**/*.scss`],
+  js: [`${DIRS.src}/js/*.js`, `${DIRS.src}/js/**/*.js`],
+  html: [`${DIRS.src}/templates/*.html`, `${DIRS.src}/templates/**/*.html`]
 };
 
 // Handle sass changes.
 gulp.task('sass', done => {
   return gulp.src(PATHS.sass)
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(concat('css/main.min.css'))
-    .pipe(minifyCss())
-    .pipe(sourcemaps.write(`../${dirs.dest}`))
-    .pipe(gulp.dest(dirs.dest))
-    .pipe(livereload())
+    // TODO fix sourcemaps
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass())
+    .pipe(plugins.concat('css/main.min.css'))
+    .pipe(plugins.cleanCss())
+    .pipe(plugins.sourcemaps.write(`../${DIRS.dest}`))
+    .pipe(gulp.dest(DIRS.dest))
+    .pipe(browserSync.stream({match: '**/*.css'}))
     .on('end', () => {
       gulp
         .src(CSS_DEPENDENCIES)
-        .pipe(sourcemaps.init())
-        .pipe(minifyCss())
-        .pipe(concat('css/styles.min.css'))
-        .pipe(sourcemaps.write(`../${dirs.dest}`))
-        .pipe(gulp.dest(dirs.dest));
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.cleanCss())
+        .pipe(plugins.concat('css/styles.min.css'))
+        .pipe(plugins.sourcemaps.write(`../${DIRS.dest}`))
+        .pipe(gulp.dest(DIRS.dest))
+        .pipe(browserSync.stream({match: '**/*.css'}))
     });
 });
 
 // Handle html changes.
 gulp.task('html', done => {
   return gulp.src(PATHS.html)
-    .pipe(gulp.dest(`${dirs.dest}/templates`))
-    .pipe(livereload());
+    .pipe(gulp.dest(`${DIRS.dest}/templates`))
+    .pipe(browserSync.stream({match: '**/*.html'}))
 });
 
 // Handle assets changes.
 gulp.task('assets', done => {
-  return gulp.src(`${dirs.src}/assets/*`)
-    .pipe(gulp.dest(`${dirs.dest}/assets`))
-    .pipe(livereload());
+  return gulp.src(`${DIRS.src}/assets/*`)
+    .pipe(gulp.dest(`${DIRS.dest}/assets`));
 });
-
-// gulp.task('annotate', () => {
-//   return gulp.src(PATHS.js)
-//     // jscs:disable
-//     .pipe(ngAnnotate({ single_quotes: true }))
-//     // jscs:enable
-//     .pipe(gulp.dest('./app/js'));
-// });
 
 // Handle js changes.
 gulp.task('js', done => {
   return gulp.src(PATHS.js)
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(concat('js/app.js'))
-  	// .pipe(ngAnnotate())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(dirs.dest))
-    .pipe(livereload())
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.babel())
+    .pipe(plugins.concat(DIRS.js.app))
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest(DIRS.dest))
+    .pipe(browserSync.stream({match: '**/*.js'}))
     .on('end', () => {
       gulp
         .src(JS_DEPENDENCIES)
-        .pipe(concat('js/libs.js'))
-        .pipe(gulp.dest(dirs.dest));
+        .pipe(plugins.concat(DIRS.js.libs))
+        .pipe(gulp.dest(DIRS.dest));
     });
 });
 
@@ -105,58 +90,49 @@ gulp.task('js', done => {
 gulp.task('minjs', done => {
   return gulp
     .src(PATHS.js)
-    .pipe(concat('js/app.js'))
-    .pipe(babel())
-    // .pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(gulp.dest(dirs.dest))
-    .pipe(livereload())
+    .pipe(plugins.concat(DIRS.js.app))
+    .pipe(plugins.babel())
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest(DIRS.dest))
     .on('end', () => {
       gulp
         .src(JS_DEPENDENCIES)
-        .pipe(concat('js/libs.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(dirs.dest));
+        .pipe(plugins.concat(DIRS.js.libs))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(DIRS.dest));
     });
 });
 
 // Start the development sever task.
 gulp.task('server', () => {
-  gulp
-    .src(dirs.dest)
-    .pipe(webserver({
-      livereload: true,
-      // host: null,
-      open: false,
-      port: process.env.PORT || 8000,
-    }));
+  browserSync({
+    notify: false,
+    server: DIRS.dest,
+    open: false,
+    tunnel: false,
+    port: 3000
+  });
 });
 
 // Task for watching file changes and livereloading the development server.
 gulp.task('watch', cb => {
-  // sequence(['sass', 'js', 'html'], 'revision', 'server', cb);
-  sequence(['sass', 'js', 'html', 'assets'], 'revision', 'server', 'watching', cb);
-  // livereload.listen();
-  // gulp.watch(PATHS.js, { interval: 1000 }, ['js']);
-  // gulp.watch(PATHS.sass, { interval: 1000 }, ['sass']);
-  // gulp.watch(PATHS.html, { interval: 1000 }, ['html']);
+  sequence(['sass', 'js', 'html', 'assets'], 'revision', ['server', 'watching'], cb);
 });
 
 gulp.task('watching', () => {
-  livereload.listen();
-  gulp.watch(PATHS.js, { interval: 1000 }, ['js']);
-  gulp.watch(PATHS.sass, { interval: 1000 }, ['sass']);
-  gulp.watch(PATHS.html, { interval: 1000 }, ['html']);
+  gulp.watch(PATHS.js, ['js', reload]);
+  gulp.watch(PATHS.sass, ['sass']);
+  gulp.watch(PATHS.html, ['html']);
 });
 
 // Gulp default task.
 gulp.task('default', ['watch']);
 
 gulp.task('revision', () => {
-  return gulp.src(`${dirs.src}/index.html`)
-    .pipe(gulp.dest(dirs.dest))
-    .pipe(rev())
-    .pipe(gulp.dest(dirs.dest));
+  return gulp.src(`${DIRS.src}/index.html`)
+    .pipe(gulp.dest(DIRS.dest))
+    .pipe(plugins.revAppend())
+    .pipe(gulp.dest(DIRS.dest));
 });
 
 // Build command
