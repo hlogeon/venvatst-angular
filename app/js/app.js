e16041b89bf0bc32b20943bfbd070b9c826e8584
Gustavo Gard <gusgard@gmail.com>
2016-03-29 21:23:41 -0300

    Refactor config

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,13 @@
 import angular from 'angular';
 import 'angular-ui-router';
+import config from './config.js';
 import './view-one/module.js';
 import './view-two/module.js';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ui.router',
-  'myApp.view-one',
-  'myApp.view-two'
-]).
-config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('view-one', {
-      url: '/view-one',
-      templateUrl: 'templates/view-one/view-one.html',
-      controller: 'ViewOneController as ViewOneCtrl'
-    })
-    .state('view-two', {
-      url: '/view-two',
-      templateUrl: 'templates/view-two/view-two.html',
-      controller: 'ViewTwoController as ViewTwoCtrl'
-    });
-  $urlRouterProvider.otherwise('view-one');
-}]);
+angular
+  .module('myApp', [
+    'ui.router',
+    'myApp.view-one',
+    'myApp.view-two'
+  ])
+  .config(config);
---
+++ app/js/config.js
@@ -0,0 +1,18 @@
+function config ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('view-one', {
+      url: '/view-one',
+      templateUrl: 'templates/view-one/view-one.html',
+      controller: 'ViewOneController as ViewOneCtrl'
+    })
+    .state('view-two', {
+      url: '/view-two',
+      templateUrl: 'templates/view-two/view-two.html',
+      controller: 'ViewTwoController as ViewTwoCtrl'
+    });
+  $urlRouterProvider.otherwise('view-one');
+};
+
+config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+export default config;
